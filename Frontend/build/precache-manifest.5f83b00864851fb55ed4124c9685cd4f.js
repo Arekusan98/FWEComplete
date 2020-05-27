d608984eb300c05c0d6de8cb2a5002405f3ffa47
Alexander <alexander8@outlook.de>
2020-05-27 22:16:11 +0200

    readme added

---
+++ Frontend/build/precache-manifest.5f83b00864851fb55ed4124c9685cd4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40885d7d7dd7a4d43a9d2bc571391a1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9042aea613776acb5ed",
+    "url": "/static/js/2.8567e5b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8567e5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f87ae7c91ada4576f01b",
+    "url": "/static/js/main.1dbadd83.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
